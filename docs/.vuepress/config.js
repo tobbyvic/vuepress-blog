a117b415f21931892a8097ec5f9b51d4b6da8e8e
闫玉东-Mac <yanyudong0904@163.com>
2020-02-11 17:13:34 +0800

    # first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+
+    themeConfig: {
+      // logo: '/',
+      // 导航栏
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Author', link: '/Author' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ],
+      // 侧边栏
+      sidebar: {
+        '/guide/': [
+          '',     /* /foo/ */
+          'front',  /* /foo/one.html */
+          'end'   /* /foo/two.html */
+        ],
+        // '/bar/': [
+        //   '',      /* /bar/ */
+        //   'three', /* /bar/three.html */
+        //   'four'   /* /bar/four.html */
+        // ],
+        // fallback 确保 fallback 侧边栏被最后定义。VuePress 会按顺序遍历侧边栏配置来寻找匹配的配置.
+        // '/': [
+        //   '',        /* / */
+        //   'contact', /* /contact.html */
+        //   'about'    /* /about.html */
+        // ]
+      }
+    }
+  }
